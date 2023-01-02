af25646f4126f6ed911c1948c45fd01cb74e2a12
Paul <paul@machine26.com>
2023-01-02 13:49:09 +0100

    Standard Redux Patterns

--- src/store.js
+++ src/store.js
@@ -47,13 +47,37 @@
 // const store = createStore(rootReducer, preloadedState)
 
 //Thunk Middleware
-import { legacy_createStore as createStore, applyMiddleware } from 'redux'
-import thunkMiddleware from 'redux-thunk'
-import { composeWithDevTools } from 'redux-devtools-extension'
-import rootReducer from './reducer'
+// import { legacy_createStore as createStore, applyMiddleware } from 'redux'
+// import thunkMiddleware from 'redux-thunk'
+// import { composeWithDevTools } from 'redux-devtools-extension'
+// import rootReducer from './reducer'
 
-const composedEnhancer = composeWithDevTools(applyMiddleware(thunkMiddleware))
+// const composedEnhancer = composeWithDevTools(applyMiddleware(thunkMiddleware))
 
-// The store now has the ability to accept thunk functions in `dispatch`
-const store = createStore(rootReducer, composedEnhancer)
+// // The store now has the ability to accept thunk functions in `dispatch`
+// const store = createStore(rootReducer, composedEnhancer)
+// export default store
+
+
+// That one call to configureStore did all the work for us:
+
+// It combined todosReducer and filtersReducer into the root reducer function, which will handle a root state that looks like {todos, filters}
+// It created a Redux store using that root reducer
+// It automatically added the thunk middleware
+// It automatically added more middleware to check for common mistakes like accidentally mutating the state
+// It automatically set up the Redux DevTools Extension connection
+
+import { configureStore } from '@reduxjs/toolkit'
+
+import todosReducer from './features/todos/todosSlice'
+import filtersReducer from './features/filters/filtersSlice'
+
+const store = configureStore({
+  reducer: {
+    // Define a top-level state field named `todos`, handled by `todosReducer`
+    todos: todosReducer,
+    filters: filtersReducer
+  }
+})
+
 export default store
